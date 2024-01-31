6f4193dd5308481833a1ffc791c2e33768098235
Sakshi Chaurasia <121746813+chaurasiasakshi@users.noreply.github.com>
2024-01-31 11:32:35 +0530

    Insert Data

--- index.js
+++ index.js
@@ -41,33 +41,102 @@ EmpSchema =  mongoose.Schema({
 
 employee= new mongoose.model('emp', EmpSchema);
 
+// Insert Multiple Entries
+
+CreateEmp = async()=>{
+
+try{
+createE1 = new employee(
+    {
+    name:"Sak",
+    mobile:9876543233,
+    age:18,
+    salary:50000,
+    married:true
+}
+
+)
+createE2 = new employee(
+    {
+    name:"Sam",
+    mobile:9876543288,
+    age:18,
+    salary:50000,
+    married:true
+}
+
+)
+createE3 = new employee(
+    {
+    name:"Sak",
+    mobile:9876543277,
+    age:18,
+    salary:80000,
+    married:false
+}
+
+)
+createE4 = new employee(
+    {
+    name:"Dam",
+    mobile:9876547810,
+    age:38,
+    salary:50000,
+    married:true
+}
+
+)
+createE5 = new employee(
+    {
+    name:"Tina",
+    mobile:9876543288,
+    age:32,
+    salary:50000,
+    married:true
+})
+empD = await employee.insertMany([createE1,createE2,createE3,createE4,createE5]);
+
+}catch (error){
+    console.log(error);
+        }
+        console.error("Employee created");
+
+}
+CreateEmp();
+// empD = await employee.insertMany([createE1,createE2,createE3,createE4,createE5]);
+
+// console.log(empD)
+
+// Insert Using Async Method
 // createEmp = async()=>{
 //     try{
-//         new employee({
+//         createE = new employee({
 //             name:"John Doe",
 //             mobile:9876543210,
 //             age:28,
 //             salary:10000,
-//             married:false
+//             married:true
 //         });
-//         console.log("Employee created");
-//         await  employee.save();
+//        empD = await createE.save();
 
 //     }catch (error){
-// console.log(error)
+// console.log(error);
 //     }
+//     console.error("Employee created");
 // }
 
-// Insert Data
+// createEmp();
 
-createEmp = new employee(
-    {
-        name:"Sakshi Chaurasia",
-        mobile:9876543210,
-        age:20,
-        salary:20000,
-        married:false
-    });
+// Insert Single  Data
 
-createEmp.save();
-console.log(`Data inserted : ${createEmp}`);
+// createEmp = new employee(
+//     {
+//         name:"Sakshi Chaurasia",
+//         mobile:9876543210,
+//         age:20,
+//         salary:20000,
+//         married:false
+//     });
+
+// createEmp.save();
+// console.log(`Data inserted : ${createEmp}`);
